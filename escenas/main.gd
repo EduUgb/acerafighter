225d24843d0617de8039f24a25c3c3be87d79101
EduUgb <carloseduardog3200@gmail.com>
2025-09-08 21:38:22 -0600

    btn

--- escenas/main.gd
+++ escenas/main.gd
@@ -3,14 +3,19 @@ extends Node2D
 @onready var final: CanvasLayer = $final
 @onready var lblt: Label = $cvtime/tiempo
 @onready var tiempo: Timer = $cvtime/Timer
+@onready var pausa: Button = $pausa
 
-var tiempoTotal: int = 6 #segundos
+var escena_actual = null
+
+var tiempoTotal: int = 60 #segundos
 
 func _ready() -> void:
 	tiempo.wait_time = 1.0   # cada segundo
 	tiempo.one_shot = false
 	tiempo.timeout.connect(_timeout)
-	iniciar_contador(6) 
+	pausa.pressed.connect(Callable(self, "_alpresionar2"))
+	escena_actual = get_tree().current_scene
+	iniciar_contador(60) 
 	
 	
 	
@@ -30,3 +35,15 @@ func _timeout():
 
 			final.mostrar()
 			await get_tree().process_frame
+			
+
+func ocultar2():
+	pausa.visible = true
+	get_tree().paused = false
+
+func _alpresionar2():
+	ocultar2()
+	await get_tree().process_frame
+	if is_instance_valid(final):
+		final.mostrar()
+		await get_tree().process_frame
